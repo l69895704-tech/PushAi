0b026304314f1c0effb21b894e598413f9ec2e34
l69895704-tech <l69895704@gmail.com>
2026-07-30 22:51:41 +0300

    Update app.dart

--- lib/lib/app.dart
+++ lib/lib/app.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
+class PushAI extends StatelessWidget {
+  const PushAI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      title: "PushAI",
+
+      theme: ThemeData(
+        brightness: Brightness.dark,
+
+        scaffoldBackgroundColor:
+            const Color(0xFF0B0B0B),
+
+        colorScheme:
+            ColorScheme.fromSeed(
+              seedColor: Colors.green,
+              brightness: Brightness.dark,
+            ),
+      ),
+
+      home: const HomeScreen(),
+    );
+  }
+}
